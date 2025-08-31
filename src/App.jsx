499d89d9b2f9217aef7f64b4f3e64183bce7514e
GlowingPiranha <ikimus01@gmail.com>
2025-08-31 17:33:59 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+function App() {
+
+  const [articoli, setArticoli] = useState([
+    {
+      id: 1,
+      title: "pokemon",
+      company: "nintendo"
+    },
+    {
+      id: 2,
+      title: "god of war",
+      company: "sony"
+    },
+    {
+      id: 3,
+      title: "halo",
+      company: "microsoft"
+    },
+    {
+      id: 4,
+      title: "the legend of zelda",
+      company: "nintendo"
+    },
+    {
+      id: 5,
+      title: "minecraft",
+      company: "mojang"
+    }
+  ]);
+
+  const [nuovoTitolo, setNuovoTitolo] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!nuovoTitolo) return;
+    const nuovoTitolo = {
+      id: articoli.length + 1,
+      title: nuovoTitolo,
+      company: "unknown"
+    };
+    setArticoli([...articoli, nuovoArticolo]);
+    setNuovoTitolo("");
+  };
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
